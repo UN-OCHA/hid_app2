595e78c108cdacc67bf7f611f4876aa0688b7257
Amy Benson <avbenson@gmail.com>
2016-11-25 15:46:15 +0000

    restructured search files

--- src/app/components/search/search.controllers.js
+++ src/app/components/search/search.controllers.js
@@ -1,3 +1,22 @@
+appControllers.controller('SearchCtrl', ['$scope', '$location', '$routeParams', 'Search',
+  function ($scope, $location, $routeParams, Search ) {
+
+  $scope.searchLists = []
+  $scope.searchPeople = []
+  $scope.searchComplete = false;
+  var searchLimit = 50;
+
+  if ($location.path() === '/search' && $routeParams.q) {
+
+    Search.UsersAndLists($routeParams.q, searchLimit).then(function(data) {
+      $scope.searchLists = data[0];
+      $scope.searchPeople = data[1];
+      $scope.searchComplete = true;
+    });
+  }
+
+}]);
+
 appControllers.controller('SearchFormCtrl', ['$rootScope', '$scope', '$location', 'Search',
   function ($rootScope, $scope, $location, Search) {
 
@@ -40,3 +59,4 @@ appControllers.controller('SearchFormCtrl', ['$rootScope', '$scope', '$location'
   });
 
 }]);
+
