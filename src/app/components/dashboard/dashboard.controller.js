ec939e2a950e9356848a526a8788a8e2f16e46b7
Guillaume Viguier <guillaume@viguierjust.com>
2016-12-09 12:35:46 +0100

    Dashboard refactoring

---
+++ src/app/components/dashboard/dashboard.controller.js
@@ -0,0 +1,38 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('app.dashboard')
+    .controller('DashboardCtrl', DashboardCtrl);
+
+  DashboardCtrl.$inject = ['$scope', '$routeParams', '$http', 'config', 'List'];
+
+  function DashboardCtrl($scope, $routeParams, $http, config, List) {
+    if ($routeParams.list) {
+      $scope.list = List.get({'listId': $routeParams.list});
+    }
+    else {
+      $scope.list = new List();
+      $scope.list.type = 'list';
+    }
+
+    // Retrieve managers
+    $scope.getManagers = function(search) {
+      $scope.newManagers = User.query({'name': search});
+    };
+
+    // Save list settings
+    $scope.listSave = function() {
+      if ($scope.list._id) {
+        $scope.list.$update(function() {
+          $location.path('/lists/' + $scope.list._id);
+        });
+      }
+      else {
+        $scope.list.$save(function() {
+        $location.path('/lists/' + $scope.list._id);
+        });
+      }
+    };
+  }
+})();
