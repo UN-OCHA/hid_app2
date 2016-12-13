5e113a3175e7ca5ab9e82c1aeadafcf7e6ef4607
Guillaume Viguier <guillaume@viguierjust.com>
2016-12-13 13:02:20 +0100

    Fixed offline caching for userCheckins

--- src/app/components/user/userCheckIn.service.js
+++ src/app/components/user/userCheckIn.service.js
@@ -9,7 +9,27 @@
 
   function UserCheckInService($cachedResource, config) {
 
-    return $cachedResource('userCheckins', config.apiUrl + 'user/:userId/:listType/:checkInId');
+    return $cachedResource('userCheckins', config.apiUrl + 'user/:userId/:listType/:checkInId', {},
+      {
+       'save': {
+         method: 'POST',
+         cache: false
+       },
+       'remove': {
+         method: 'DELETE',
+         cache: false
+       },
+       'delete': {
+         method: 'DELETE',
+         cache: false
+       },
+       'update': {
+         method: 'PUT',
+           // TODO: find a way to cache these requests, and fix https://github.com/goodeggs/angular-cached-resource/issues/72
+           cache: false
+         }
+       }
+     );
 
   }
 
