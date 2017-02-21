17b79ca4df744757a1091b946f98b6a63b48e153
Amy Benson <avbenson@gmail.com>
2017-02-21 10:32:52 +0000

    Updated alert styles

--- Gruntfile.js
+++ Gruntfile.js
@@ -84,7 +84,7 @@ module.exports = function(grunt) {
     watch: {
       sass: {
         files: ['src/app/common/**/*.scss', 'src/app/components/**/*.scss'],
-        tasks: ['sass_import', 'concat', 'sass'],
+        tasks: ['sass_import', 'concat', 'sass:dist'],
         options: {
           spawn: false,
         }
