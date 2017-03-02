645e3ca90553711610842c4bd9facdfa98991403
Amy Benson <avbenson@gmail.com>
2017-03-02 16:56:00 +0000

    Use service worker for notifications for Chrome mobile

---
+++ src/sw.js
@@ -0,0 +1,21 @@
+'use strict';
+
+self.addEventListener('notificationclick', function(event) {
+  
+  event.notification.close();
+
+  event.waitUntil(clients.matchAll({
+    type: 'window'
+  }).then(function(clientList) {
+    for (var i = 0; i < clientList.length; i++) {
+      var client = clientList[i];
+
+      if (client.url === '/' && 'focus' in client) {
+        return client.focus();
+      }
+    }
+    if (clients.openWindow) {
+      return clients.openWindow('/');
+    }
+  }));
+});
