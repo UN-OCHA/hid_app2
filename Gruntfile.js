b7fbd47804b7e8970c5ac95b850af8ce5c3d96ff
Amy Benson <avbenson@gmail.com>
2017-03-17 10:41:33 +0000

    Cache icons

--- Gruntfile.js
+++ Gruntfile.js
@@ -167,6 +167,7 @@ module.exports = function(grunt) {
           basePath: 'dist/',
           cache: ['<%= cacheHash["js/app.min.js"] %>',
                   '<%= cacheHash["css/app.min.css"] %>',
+                  '<%= cacheHash["fonts/icons/*.*"] %>',
                   '/favicon.ico'],
           // network: ['*'],
           //fallback: ['/ partials/offline.html'],
