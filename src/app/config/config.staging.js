667d1e6dac891141e833c066a6144e21dcbef516
Guillaume Viguier <guillaume@viguierjust.com>
2018-03-07 15:58:52 +0100

    Added config for staging environment

---
+++ src/app/config/config.staging.js
@@ -0,0 +1,16 @@
+(function (window) {
+  window.__env = window.__env || {};
+
+  // API url
+  window.__env.apiUrl = 'https://api.staging.humanitarian.id/api/v2/';
+
+  // Hrinfo url
+  window.__env.hrinfoUrl = 'https://www.humanitarianresponse.info/en/api/v1.0/';
+
+  // List type
+  window.__env.listTypes = ['operation', 'bundle', 'disaster', 'organization', 'list', 'functional_role', 'office'];
+
+  //Google Analytics tracking ID
+  window.__env.gaTrackingId = 'UA-XXXXXXXX-X'
+
+}(this));
