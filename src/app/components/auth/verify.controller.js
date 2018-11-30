11005fecade942fd165ed21b75cf78ea7c0e59f8
Guillaume Viguier <guillaume@viguierjust.com>
2018-11-30 11:02:37 +0100

    HID-1707 - changed notification text after email validation

--- src/app/components/auth/verify.controller.js
+++ src/app/components/auth/verify.controller.js
@@ -10,10 +10,10 @@
   function VerifyCtrl ($scope, $location, $routeParams, User, gettextCatalog, alertService) {
     var hash = $location.search().hash;
     User.validateEmail(hash, function (response) {
-      alertService.add('success', gettextCatalog.getString('Thank you for verifying your email. You can now login through our application, or through any other application using Humanitarian ID'));
+      alertService.add('success', gettextCatalog.getString('Thank you for confirming your email. You can now login through our application, or through any other application using Humanitarian ID'));
       $location.path('/');
     }, function (response) {
-      alertService.add('danger', gettextCatalog.getString('The verification link did not work'));
+      alertService.add('danger', gettextCatalog.getString('The confirmation link did not work'));
     });
   }
 })();
