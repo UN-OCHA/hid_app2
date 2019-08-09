7096544d5b5cfb5aab905d590a99555752e6fd3f
Chris Ruppel <chris@humanitarianresponse.info>
2019-08-09 13:06:12 +0200

    HID-1824: ResetPasswordController angular/controller-as

--- src/app/components/auth/ResetPasswordController.js
+++ src/app/components/auth/ResetPasswordController.js
@@ -8,24 +8,25 @@
   ResetPasswordController.$inject = ['$scope', '$routeParams', '$location', 'alertService', 'TwoFactorAuthService', 'User', 'gettextCatalog'];
 
   function ResetPasswordController ($scope, $routeParams, $location, alertService, TwoFactorAuthService, User, gettextCatalog) {
-    $scope.isOrphan = $routeParams.orphan;
-    $scope.resetPassword = resetPassword;
-    $scope.requestPasswordReset = requestPasswordReset;
+    var thisScope = $scope;
+    thisScope.isOrphan = $routeParams.orphan;
+    thisScope.resetPassword = resetPassword;
+    thisScope.requestPasswordReset = requestPasswordReset;
 
     function requestPasswordReset () {
       var app_reset_url = $location.protocol() + '://' + $location.host() + '/reset_password';
-      User.passwordReset($scope.email, function (response) {
-        alertService.add('success', gettextCatalog.getString('Password reset was sent to ' + $scope.email + '. Please make sure the email address is correct. If not, please reset your password again'));
-        $scope.reset.$setPristine();
+      User.passwordReset(thisScope.email, function (response) {
+        alertService.add('success', gettextCatalog.getString('Password reset was sent to ' + thisScope.email + '. Please make sure the email address is correct. If not, please reset your password again'));
+        thisScope.reset.$setPristine();
       }, function (error) {
         var msg = gettextCatalog.getString('There was an error resetting your password. Please try again or contact the HID team.');
         alertService.add('danger', msg);
-        $scope.reset.$setPristine();
+        thisScope.reset.$setPristine();
       });
     }
 
     function resetPassword (token) {
-      User.resetPassword($location.search().hash, $location.search().id, $location.search().time, $scope.newPassword, function (response) {
+      User.resetPassword($location.search().hash, $location.search().id, $location.search().time, thisScope.newPassword, function (response) {
         alertService.add('success', gettextCatalog.getString('Your password was successfully changed. You can now login.'));
         $location.path('/');
       }, function (error) {
@@ -33,12 +34,12 @@
           TwoFactorAuthService.requestToken(function(token) {
             resetPassword(token);
           }, function () {
-            $scope.resetPasswordForm.$setPristine();
+            thisScope.resetPasswordForm.$setPristine();
           });
           return;
         }
         alertService.add('danger', gettextCatalog.getString('There was an error resetting your password. Please try again or contact the HID team.'));
-        $scope.resetPasswordForm.$setPristine();
+        thisScope.resetPasswordForm.$setPristine();
       }, token);
     }
 
