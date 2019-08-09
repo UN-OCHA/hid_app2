4c54e19277892cf374f7e915d4c446c1936feec5
Chris Ruppel <chris@humanitarianresponse.info>
2019-08-09 13:11:10 +0200

    HID-1824: ClientController angular/controller-as

--- src/app/components/client/ClientController.js
+++ src/app/components/client/ClientController.js
@@ -8,30 +8,31 @@
   ClientController.$inject = ['$exceptionHandler', '$scope', '$routeParams', '$http', '$window', 'gettextCatalog', 'alertService', 'Client'];
 
   function ClientController ($exceptionHandler, $scope, $routeParams, $http, $window, gettextCatalog, alertService, Client) {
+    var thisScope = $scope;
     if ($routeParams.clientId) {
-      $scope.client = Client.get({'clientId': $routeParams.clientId});
+      thisScope.client = Client.get({'clientId': $routeParams.clientId});
     }
     else {
-      $scope.client = new Client();
+      thisScope.client = new Client();
     }
 
-    $scope.saveClient = function() {
+    thisScope.saveClient = function() {
       var success = function (resp, headers) {
         alertService.add('success', gettextCatalog.getString('Client saved successfully'));
       };
       var error = function (err) {
         $exceptionHandler(error, 'Save client');
       };
-      if ($scope.client._id) {
-        $scope.client.$update(success, error);
+      if (thisScope.client._id) {
+        thisScope.client.$update(success, error);
       }
       else {
-        $scope.client.$save(success, error);
+        thisScope.client.$save(success, error);
       }
     };
 
-    $scope.deleteClient = function () {
-      $scope.client.$delete(function (resp, headers) {
+    thisScope.deleteClient = function () {
+      thisScope.client.$delete(function (resp, headers) {
         alertService.add('success', gettextCatalog.getString('Client deleted successfully'));
       });
     };
