5d54db4298642a1bf7f8ef46e70ee7716f9daaa0
Chris Ruppel <chris@humanitarianresponse.info>
2019-08-09 14:28:48 +0200

    HID-1824: UserPrefsController angular/controller-as

--- src/app/components/user/UserPrefsController.js
+++ src/app/components/user/UserPrefsController.js
@@ -8,26 +8,28 @@
   UserPrefsController.$inject = ['$exceptionHandler', '$scope', '$location', '$timeout', 'AuthService', 'alertService', 'UserDataService', 'gettextCatalog', 'TwoFactorAuthService', 'FileSaver', 'Blob'];
 
   function UserPrefsController($exceptionHandler, $scope, $location, $timeout, AuthService, alertService, UserDataService, gettextCatalog, TwoFactorAuthService, FileSaver, Blob) {
-    $scope.pendingConnections = [];
-    $scope.approvedConnections = [];
-    $scope.password = {
+    var thisScope = $scope;
+
+    thisScope.pendingConnections = [];
+    thisScope.approvedConnections = [];
+    thisScope.password = {
       old: '',
       new: ''
     };
 
-    $scope.timezones = moment.tz.names();
-    $scope.twoFactorAuthStep = 1;
-    $scope.showMore = false;
+    thisScope.timezones = moment.tz.names();
+    thisScope.twoFactorAuthStep = 1;
+    thisScope.showMore = false;
 
-    UserDataService.getUserFromServer($scope.currentUser.id)
+    UserDataService.getUserFromServer(thisScope.currentUser.id)
     .then(function (user) {
-      $scope.user = user;
-      $scope.trustedDevices = parseTrustedDevices($scope.user.totpTrusted);
-      getConnections($scope.user);
+      thisScope.user = user;
+      thisScope.trustedDevices = parseTrustedDevices(thisScope.user.totpTrusted);
+      getConnections(thisScope.user);
     });
 
     function updateUserPassword (form, token) {
-      $scope.user.changePassword($scope.user, function () {
+      thisScope.user.changePassword(thisScope.user, function () {
         alertService.add('success', gettextCatalog.getString('Your password was successfully changed.'));
         form.$setPristine();
       }, function (error) {
@@ -37,11 +39,11 @@
     }
 
     // Set a new password for the current user
-    $scope.savePassword = function(form) {
-      $scope.user.old_password = $scope.password.old;
-      $scope.user.new_password = $scope.password.new;
+    thisScope.savePassword = function(form) {
+      thisScope.user.old_password = thisScope.password.old;
+      thisScope.user.new_password = thisScope.password.new;
 
-      if ($scope.user.totp) {
+      if (thisScope.user.totp) {
         TwoFactorAuthService.requestToken(function (token) {
           updateUserPassword(form, token);
         }, function () {
@@ -54,40 +56,40 @@
     };
 
     // Set settings for the current user
-    $scope.saveSettings = function () {
-      $scope.user.$update(function () {
+    thisScope.saveSettings = function () {
+      thisScope.user.$update(function () {
         alertService.add('success', gettextCatalog.getString('Your settings were successfully changed.'));
-        $scope.setCurrentUser($scope.user);
-        $scope.initLanguage();
+        thisScope.setCurrentUser(thisScope.user);
+        thisScope.initLanguage();
       }, function (error) {
         $exceptionHandler(error, 'saveSettings');
       });
     };
 
     // Delete current user account
-    $scope.deleteAccount = function () {
+    thisScope.deleteAccount = function () {
       alertService.add('danger', gettextCatalog.getString('Are you sure you want to do this ? You will not be able to access Humanitarian ID anymore.'), true, function() {
-        $scope.user.$delete(function () {
+        thisScope.user.$delete(function () {
           alertService.add('success', gettextCatalog.getString('Your account was successfully removed. You are now logged out. Sorry to have you go.'), false, function () {});
           AuthService.logout();
-          $scope.removeCurrentUser();
+          thisScope.removeCurrentUser();
           $location.path('/');
         });
       });
     };
 
     // Revoke client
-    $scope.revokeClient = function (client) {
+    thisScope.revokeClient = function (client) {
       alertService.add('danger', gettextCatalog.getString('Are you sure you want to do this? You will need to authorize this application again to access it through Humanitarian ID.'), true, function () {
         var index = -1;
-        for (var i = 0, len = $scope.user.authorizedClients.length; i < len; i++) {
-          if ($scope.user.authorizedClients[i].id == client.id) {
+        for (var i = 0, len = thisScope.user.authorizedClients.length; i < len; i++) {
+          if (thisScope.user.authorizedClients[i].id == client.id) {
             index = i;
           }
         }
         if (index != -1) {
-          $scope.user.authorizedClients.splice(index, 1);
-          $scope.user.$update(function () {
+          thisScope.user.authorizedClients.splice(index, 1);
+          thisScope.user.$update(function () {
             alertService.add('success', gettextCatalog.getString('Application successfully revoked.'));
           }, function (error) {
             $exceptionHandler(error, 'revokeClient');
@@ -97,93 +99,93 @@
     };
 
     function getConnections (user) {
-      $scope.pendingConnections = user.connections.filter(function(connection) {
+      thisScope.pendingConnections = user.connections.filter(function(connection) {
         return connection.pending;
       });
 
-      $scope.approvedConnections = user.connections.filter(function(connection) {
+      thisScope.approvedConnections = user.connections.filter(function(connection) {
         return !connection.pending;
       });
     }
 
-    $scope.approveConnection = function (connection) {
-      $scope.user.approveConnection($scope.user._id, connection._id, function () {
+    thisScope.approveConnection = function (connection) {
+      thisScope.user.approveConnection(thisScope.user._id, connection._id, function () {
         alertService.add('success', 'Connection approved', false, function () {});
-        var index = $scope.user.connections.indexOf(connection);
-        $scope.user.connections[index].pending = false;
-        getConnections($scope.user);
-        $scope.setCurrentUser($scope.user);
+        var index = thisScope.user.connections.indexOf(connection);
+        thisScope.user.connections[index].pending = false;
+        getConnections(thisScope.user);
+        thisScope.setCurrentUser(thisScope.user);
       }, function (error) {
         $exceptionHandler(error, 'approveConnection');
       });
     };
 
-    $scope.removeConnection = function (connection) {
-      $scope.user.deleteConnection($scope.user._id, connection._id, function () {
+    thisScope.removeConnection = function (connection) {
+      thisScope.user.deleteConnection(thisScope.user._id, connection._id, function () {
         alertService.add('success', gettextCatalog.getString('Connection removed'), false, function () {});
-        $scope.user.connections.splice($scope.user.connections.indexOf(connection), 1);
-        getConnections($scope.user);
-        $scope.setCurrentUser($scope.user);
+        thisScope.user.connections.splice(thisScope.user.connections.indexOf(connection), 1);
+        getConnections(thisScope.user);
+        thisScope.setCurrentUser(thisScope.user);
       }, function (error) {
         $exceptionHandler(error, 'removeConnection');
       });
     };
 
-    $scope.tokens = [];
+    thisScope.tokens = [];
     AuthService.getUserTokens(function (tokens) {
       angular.forEach(tokens, function (token) {
         if (!token.blacklist) {
-          $scope.tokens.push(token);
+          thisScope.tokens.push(token);
         }
       });
     });
 
-    $scope.newToken = function () {
+    thisScope.newToken = function () {
       AuthService.generateAPIToken(function (token) {
         token.new = true;
-        $scope.tokens.unshift(token);
+        thisScope.tokens.unshift(token);
       });
     };
 
-    $scope.deleteToken = function (token) {
+    thisScope.deleteToken = function (token) {
       AuthService.deleteToken(token.token, function () {
         alertService.add('success', gettextCatalog.getString('API key deleted'), false, function () {});
-        $scope.tokens.splice($scope.tokens.indexOf(token), 1);
+        thisScope.tokens.splice(thisScope.tokens.indexOf(token), 1);
       });
     };
 
     // Two Factor Auth
-    $scope.getQRCode = function () {
+    thisScope.getQRCode = function () {
       TwoFactorAuthService.generateQRCode(function (response) {
-        $scope.qrCode = response.data.url;
-        $scope.twoFactorAuthStep = 2;
+        thisScope.qrCode = response.data.url;
+        thisScope.twoFactorAuthStep = 2;
       }, function (error) {
         $exceptionHandler(error, 'getQRCode');
       });
     };
 
-    $scope.getRecoveryCodes = function () {
+    thisScope.getRecoveryCodes = function () {
       TwoFactorAuthService.generateRecoveryCodes(function (response) {
-        $scope.recoveryCodes = response.data;
+        thisScope.recoveryCodes = response.data;
       }, function (error) {
         $exceptionHandler(error, 'getRecoveryCodes');
       });
     };
 
-    $scope.downloadRecoveryCodes = function () {
+    thisScope.downloadRecoveryCodes = function () {
       var codes = [];
-      angular.forEach($scope.recoveryCodes, function (code) {
+      angular.forEach(thisScope.recoveryCodes, function (code) {
         codes.push(code + '\r\n');
       });
       var data = new Blob(codes, { type: 'text/plain;charset=utf-8' });
       FileSaver.saveAs(data, 'hid-recovery-codes.txt');
     };
 
-    $scope.enableTFA = function (token) {
+    thisScope.enableTFA = function (token) {
       TwoFactorAuthService.enable(token, function (response) {
-        $scope.twoFactorAuthStep = 3;
-        $scope.setCurrentUser(response.data);
-        $scope.getRecoveryCodes();
+        thisScope.twoFactorAuthStep = 3;
+        thisScope.setCurrentUser(response.data);
+        thisScope.getRecoveryCodes();
       }, function (error) {
         $exceptionHandler(error, 'enableTFA');
       });
@@ -191,31 +193,31 @@
 
     function disableTFA (token) {
       TwoFactorAuthService.disable(token, function (response) {
-        $scope.setCurrentUser(response.data);
-        $scope.user.totp = false;
-        $scope.recoveryCodes = [];
+        thisScope.setCurrentUser(response.data);
+        thisScope.user.totp = false;
+        thisScope.recoveryCodes = [];
       }, function (error) {
         $exceptionHandler(error, 'disableTFA');
       });
     }
 
-    $scope.disableTwoFactorAuth = function () {
+    thisScope.disableTwoFactorAuth = function () {
       TwoFactorAuthService.requestToken(function (token) {
         disableTFA(token);
       });
     };
 
-    $scope.resetTFAForm = function () {
-      $scope.user.totp = true;
-      $scope.twoFactorAuthStep = 1;
-      $scope.recoveryCodes = [];
+    thisScope.resetTFAForm = function () {
+      thisScope.user.totp = true;
+      thisScope.twoFactorAuthStep = 1;
+      thisScope.recoveryCodes = [];
     };
 
-    $scope.deleteTrustedDevice = function (id) {
+    thisScope.deleteTrustedDevice = function (id) {
       TwoFactorAuthService.deleteTrustedDevice(id, function () {
         alertService.add('success', gettextCatalog.getString('Device removed.'));
-        var index = $scope.trustedDevices.map(function(x){ return x._id; }).indexOf(id);
-        $scope.trustedDevices.splice(index,1);
+        var index = thisScope.trustedDevices.map(function(x){ return x._id; }).indexOf(id);
+        thisScope.trustedDevices.splice(index,1);
       }, function (error) {
          $exceptionHandler(error, 'delete Trusted Device');
       });
