94b6081b6d416b8ea9cd3da60a83b1a9a177356a
Chris Ruppel <chris@humanitarianresponse.info>
2019-08-09 13:14:46 +0200

    HID-1824: LandingController angular/controller-as

--- src/app/components/landing/LandingController.js
+++ src/app/components/landing/LandingController.js
@@ -8,29 +8,30 @@
   LandingController.$inject = ['$location', '$scope', 'notificationsService'];
 
   function LandingController($location, $scope, notificationsService) {
-    $scope.notifications = notificationsService;
+    var thisScope = $scope;
+    thisScope.notifications = notificationsService;
 
-    $scope.recentUserSearches = [];
-    $scope.recentListSearches = [];
-    $scope.recentOperationSearches = [];
+    thisScope.recentUserSearches = [];
+    thisScope.recentListSearches = [];
+    thisScope.recentOperationSearches = [];
 
-    if ($scope.currentUser.appMetadata && $scope.currentUser.appMetadata.hid && $scope.currentUser.appMetadata.hid.recentSearches) {
+    if (thisScope.currentUser.appMetadata && thisScope.currentUser.appMetadata.hid && thisScope.currentUser.appMetadata.hid.recentSearches) {
 
-      if ($scope.currentUser.appMetadata.hid.recentSearches.user) {
-        $scope.recentUserSearches = $scope.currentUser.appMetadata.hid.recentSearches.user;
+      if (thisScope.currentUser.appMetadata.hid.recentSearches.user) {
+        thisScope.recentUserSearches = thisScope.currentUser.appMetadata.hid.recentSearches.user;
       }
 
-      if ($scope.currentUser.appMetadata.hid.recentSearches.list) {
-        $scope.recentListSearches = $scope.currentUser.appMetadata.hid.recentSearches.list;
+      if (thisScope.currentUser.appMetadata.hid.recentSearches.list) {
+        thisScope.recentListSearches = thisScope.currentUser.appMetadata.hid.recentSearches.list;
       }
 
-      if ($scope.currentUser.appMetadata.hid.recentSearches.operation) {
-        $scope.recentOperationSearches = $scope.currentUser.appMetadata.hid.recentSearches.operation;
+      if (thisScope.currentUser.appMetadata.hid.recentSearches.operation) {
+        thisScope.recentOperationSearches = thisScope.currentUser.appMetadata.hid.recentSearches.operation;
       }
 
     }
 
-    $scope.readNotification = function (notification) {
+    thisScope.readNotification = function (notification) {
       notification.notified = true;
       notification.read = true;
       notificationsService.update(notification);
