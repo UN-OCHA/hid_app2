8a31efbf552a51a193ed6797329f24c9883cb535
Chris Ruppel <chris@humanitarianresponse.info>
2019-08-09 14:24:48 +0200

    HID-1824: OperationsController angular/controller-as

--- src/app/components/operations/OperationsController.js
+++ src/app/components/operations/OperationsController.js
@@ -8,33 +8,35 @@
   OperationsController.$inject = ['$scope', '$routeParams', 'Operation'];
 
   function OperationsController ($scope, $routeParams, Operation) {
-    $scope.pagination = {
+    var thisScope = $scope;
+
+    thisScope.pagination = {
       currentPage: 1,
       itemsPerPage: 10,
       totalItems: 0
     };
 
     var setTotalOperations = function (operations, headers) {
-      $scope.pagination.totalItems = headers()["x-total-count"];
+      thisScope.pagination.totalItems = headers()["x-total-count"];
     };
 
     function getOperations (offset) {
       var params = {
         sort: 'name',
-        limit: $scope.pagination.itemsPerPage
+        limit: thisScope.pagination.itemsPerPage
       };
       params.offset = offset || 0;
 
       Operation.query(params, function (operations, headers) {
         setTotalOperations(operations, headers);
-        $scope.operations = operations;
+        thisScope.operations = operations;
       });
     }
 
     getOperations();
 
-    $scope.pageChanged = function () {
-      var offset = $scope.pagination.itemsPerPage * ($scope.pagination.currentPage - 1);
+    thisScope.pageChanged = function () {
+      var offset = thisScope.pagination.itemsPerPage * (thisScope.pagination.currentPage - 1);
       getOperations(offset);
     };
 
