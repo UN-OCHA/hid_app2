8884c355c78a62ae91ff9715a8f5ae61e2c3cc4a
Chris Ruppel <chris@humanitarianresponse.info>
2019-08-09 13:27:52 +0200

    HID-1824: OutlookController angular/controller-as

--- src/app/components/outlook/OutlookController.js
+++ src/app/components/outlook/OutlookController.js
@@ -8,11 +8,12 @@
   OutlookController.$inject = ['$scope', '$location', '$routeParams', 'User', 'gettextCatalog', 'alertService'];
 
   function OutlookController ($scope, $location, $routeParams, User, gettextCatalog, alertService) {
+    var thisScope = $scope;
 
     var hash = $location.hash().split('&');
     if (hash[0].indexOf('code') === 0) {
       var code = hash[0].split('=');
-      $scope.currentUser.saveOutlookCredentials(code[1])
+      thisScope.currentUser.saveOutlookCredentials(code[1])
         .then(function (resp) {
           $location.path('/');
         });
