73aca97f8f0f458965b7be44ff238197570a5231
Chris Ruppel <chris@humanitarianresponse.info>
2019-08-23 11:07:08 +0200

    HID-1831: tabs -> spaces

--- src/e2e-tests/specs/lists/new-list-spec.js
+++ src/e2e-tests/specs/lists/new-list-spec.js
@@ -6,76 +6,76 @@ var NewListPage = require('../../pages/new-list-page');
 var ListPage = require('../../pages/list-page');
 
 describe('New Lists', function () {
-	var loginPage = new LoginPage();
-	var dashboardPage = new DashboardPage();
-	var newListPage = new NewListPage();
-	var listPage = new ListPage();
-	var navObject = new NavObject();
+  var loginPage = new LoginPage();
+  var dashboardPage = new DashboardPage();
+  var newListPage = new NewListPage();
+  var listPage = new ListPage();
+  var navObject = new NavObject();
 
-	beforeAll(function () {
-		loginPage.get();
-	  loginPage.login();
-	});
+  beforeAll(function () {
+    loginPage.get();
+    loginPage.login();
+  });
 
-	describe('Creating and deleting a list', function () {
+  describe('Creating and deleting a list', function () {
 
-		beforeAll(function () {
-			navObject.dashboardLink.click();
-			browser.wait(dashboardPage.pageHeading.isDisplayed(), 10000);
-	  	dashboardPage.createListButton.click();
-	  	browser.wait(newListPage.pageHeading.isDisplayed(), 10000);
-		});
+    beforeAll(function () {
+      navObject.dashboardLink.click();
+      browser.wait(dashboardPage.pageHeading.isDisplayed(), 10000);
+      dashboardPage.createListButton.click();
+      browser.wait(newListPage.pageHeading.isDisplayed(), 10000);
+    });
 
-		describe('Creating a new list', function () {
+    describe('Creating a new list', function () {
 
-			it('should go to the create list page', function () {
-				expect(browser.getCurrentUrl()).toBe(browser.baseUrl + 'lists/new');
-			});
+      it('should go to the create list page', function () {
+        expect(browser.getCurrentUrl()).toBe(browser.baseUrl + 'lists/new');
+      });
 
-			it('should save and go to the new list', function () {
-				newListPage.populateNewListForm();
-				expect(listPage.listTitle.getText()).toBe(browser.params.tempList);
-			});
+      it('should save and go to the new list', function () {
+        newListPage.populateNewListForm();
+        expect(listPage.listTitle.getText()).toBe(browser.params.tempList);
+      });
 
-			it('should show on the dashboard', function () {
-				navObject.dashboardLink.click();
-				browser.wait(dashboardPage.pageHeading.isDisplayed(), 10000);
-				dashboardPage.listsManageTabBtn.click();
-				expect(dashboardPage.listsManage.getText()).toContain(browser.params.tempList);
-			});
+      it('should show on the dashboard', function () {
+        navObject.dashboardLink.click();
+        browser.wait(dashboardPage.pageHeading.isDisplayed(), 10000);
+        dashboardPage.listsManageTabBtn.click();
+        expect(dashboardPage.listsManage.getText()).toContain(browser.params.tempList);
+      });
 
-		});
+    });
 
-		describe('Deleting the list', function () {
+    describe('Deleting the list', function () {
 
-			beforeAll(function () {
-				listPage.goToList(browser.params.tempList);
-				listPage.openListAdmin();
-				browser.wait(listPage.deleteButton.isDisplayed(), 10000);
-				listPage.deleteButton.click();
-			});
+      beforeAll(function () {
+        listPage.goToList(browser.params.tempList);
+        listPage.openListAdmin();
+        browser.wait(listPage.deleteButton.isDisplayed(), 10000);
+        listPage.deleteButton.click();
+      });
 
-			it('should ask for confirmation', function () {
-				browser.wait(listPage.confirmModal.isDisplayed(), 10000);
-				expect(listPage.confirmModal.isPresent()).toBeTruthy();
-				listPage.confirmModalButton.click();
-			});
+      it('should ask for confirmation', function () {
+        browser.wait(listPage.confirmModal.isDisplayed(), 10000);
+        expect(listPage.confirmModal.isPresent()).toBeTruthy();
+        listPage.confirmModalButton.click();
+      });
 
-			it('should redirect to the lists page', function () {
-				expect(browser.getCurrentUrl()).toBe(browser.baseUrl + 'lists');
-			});
+      it('should redirect to the lists page', function () {
+        expect(browser.getCurrentUrl()).toBe(browser.baseUrl + 'lists');
+      });
 
-			it('should show the success message', function () {
-				browser.wait(listPage.deleteSuccessModalText.isDisplayed(), 10000);
-				expect(listPage.deleteSuccessModalText.isPresent()).toBeTruthy();
-			});
-		});
+      it('should show the success message', function () {
+        browser.wait(listPage.deleteSuccessModalText.isDisplayed(), 10000);
+        expect(listPage.deleteSuccessModalText.isPresent()).toBeTruthy();
+      });
+    });
 
-	});
+  });
 
-	afterAll(function () {
-		browser.sleep(3000); //wait for modals to close
-		navObject.logOut();
-	});
+  afterAll(function () {
+    browser.sleep(3000); //wait for modals to close
+    navObject.logOut();
+  });
 
 });
