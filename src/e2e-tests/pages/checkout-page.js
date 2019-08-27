f3a69efb712c21a869a5123afc19379851045aa8
Chris Ruppel <chris@humanitarianresponse.info>
2019-08-27 12:29:33 +0200

    HID-1838: tabs -> spaces

--- src/e2e-tests/pages/checkout-page.js
+++ src/e2e-tests/pages/checkout-page.js
@@ -1,15 +1,15 @@
 /* jshint module: true */
 var CheckoutPage = function() {
-	this.pageHeading = element(by.cssContainingText('.page-header__heading', 'Check out'));
-	this.lists = element.all(by.css('.t-list a'));
-	this.listToCheckoutOf = element(by.cssContainingText('.t-list', browser.params.standardTestList));
-	this.checkoutButton = this.listToCheckoutOf.element(by.css('.t-checkout-btn'));
-	this.modalOverlay = element(by.css('.modal'));
-	this.confirmModal = element(by.css('.modal-dialog'));
-	this.confirmModalText = element(by.cssContainingText('div .modal-body', 'Are you sure?'));
-	this.confirmModalButton = element(by.css('.t-confirm-btn'));
-	this.successModal = element(by.css('.modal-success'));
-	this.successModalText = element(by.cssContainingText('div .modal-body', 'Successfully removed from list'));
+  this.pageHeading = element(by.cssContainingText('.page-header__heading', 'Check out'));
+  this.lists = element.all(by.css('.t-list a'));
+  this.listToCheckoutOf = element(by.cssContainingText('.t-list', browser.params.standardTestList));
+  this.checkoutButton = this.listToCheckoutOf.element(by.css('.t-checkout-btn'));
+  this.modalOverlay = element(by.css('.modal'));
+  this.confirmModal = element(by.css('.modal-dialog'));
+  this.confirmModalText = element(by.cssContainingText('div .modal-body', 'Are you sure?'));
+  this.confirmModalButton = element(by.css('.t-confirm-btn'));
+  this.successModal = element(by.css('.modal-success'));
+  this.successModalText = element(by.cssContainingText('div .modal-body', 'Successfully removed from list'));
 };
 
 module.exports = CheckoutPage;
